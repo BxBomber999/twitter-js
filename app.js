75743a31c59586bced4ba5636677f7101139b80a
andrew wong <andrew@andrews-MacBook-Pro.local>
2018-03-10 16:54:54 -0500

    up to live test

--- app.js
+++ app.js
@@ -2,27 +2,25 @@ const express = require('express');
 const app = express();
 // template file + locals object -> rendering function -> complete html
 const nunjucks = require('nunjucks');
+const morgan = require('morgan');
 
 //middle-ware fires with every incoming request and helps log helpful messages
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+app.set('view engine', 'html'); // tells express res.render will have html files
+app.engine('html', nunjucks.render); // tells express to use nunjucks res.render
+
+app.use(morgan('dev'));
+
 app.use(function(req, res, next){
   console.log('Request type: ', req.method, req.path );
   next();
 })
 
-nunjucks.configure('views', {noCache: true }); // point nunjucks to the proper directory for templates
-app.set('view engine', 'html'); // tells express res.render will have html files
-app.engine('html', nunjucks.render); // tells express to use nunjucks res.render
-
-
 const routes = require('./routes');
 app.use('/', routes);
 
-
-
-
-
-
-
+// console.log(__dirname + '/public');
+app.use(express.static(__dirname + '/public'));
 
 
 app.listen(3000, function(){
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetBank.js');
 
-
 router.get('/', function(req, res, next){
   let tweets = tweetBank.list();
   res.render( 'index', {title: 'Twitter clone!', tweets : tweets} );
-})
-console.log(router);
+});
 
 module.exports = router;
